a764268587a8365363683d07b3200069aaa8895b
Pavel Kladkevich <kladpav@gmail.com>
2023-11-11 23:10:07 +0300

    add feature actions for administrator

--- src/components/ModalDeleteQuote.js
+++ src/components/ModalDeleteQuote.js
@@ -3,31 +3,36 @@ import Modal from 'react-bootstrap/Modal';
 
 import { useStorage } from '../firebase/storage';
 
-function ModalDeleteQuote({showModalDelete, setShowModalDelete, quote}) {
+function ModalDeleteQuote({showModalDelete, setShowModalDelete, quote, changeParameter, favorite}) {
 
-  const {changeFavQuotes} = useStorage();
+  const {changeFavQuotes, changeAllQuotes} = useStorage();
   const handleClose = () => {
+    changeParameter(null);
     setShowModalDelete(false);
   }
   const checkForm = () => {
     setShowModalDelete(false);
-    changeFavQuotes(quote, 'delete');
+    if (favorite) {
+      changeFavQuotes(quote, 'delete');
+    } else {
+      changeAllQuotes(quote, 'delete');
+    }
   }
 
   return (
-      <Modal className='my-5' show={showModalDelete} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title className='fs-5'>Вы действительно хотите удалить цитату ?</Modal.Title>
-        </Modal.Header>
-        <Modal.Footer>
-          <Button variant="outline-info" className='bg-white' onClick={handleClose}>
-            НЕТ
-          </Button>
-          <Button type="submit" variant="info" className='text-white' onClick={checkForm}>
-            УДАЛИТЬ
-          </Button>
-        </Modal.Footer>
-      </Modal>
+    <Modal className='my-5' show={showModalDelete} onHide={handleClose}>
+      <Modal.Header closeButton>
+        <Modal.Title className='fs-5'>Вы действительно хотите удалить цитату ?</Modal.Title>
+      </Modal.Header>
+      <Modal.Footer>
+        <Button variant="outline-info" className='bg-white' onClick={handleClose}>
+          НЕТ
+        </Button>
+        <Button type="submit" variant="info" className='text-white' onClick={checkForm}>
+          УДАЛИТЬ
+        </Button>
+      </Modal.Footer>
+    </Modal>
   );
 }
 
